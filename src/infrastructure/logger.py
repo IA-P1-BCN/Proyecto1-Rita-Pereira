fe524eac64af20f5b8a618d88300384ef83d9bfb
RitaP <ritaqpereira@gmail.com>
2026-09-22 15:40:17 +0200

    Add logger and integrate it into cli.py; stop tracking runtime data

---
+++ src/infrastructure/logger.py
@@ -0,0 +1,13 @@
+import logging
+import os
+
+def configurar_logger():
+    os.makedirs("logs", exist_ok=True)
+
+    logging.basicConfig(
+        filename="logs/taximetro.log",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+    return logging.getLogger("taximetro")
--- src/interfaces/cli.py
+++ src/interfaces/cli.py
@@ -1,6 +1,7 @@
 from src.domain.carrera import Carrera
 from src.domain.tarifa import Tarifa
 from src.infrastructure.historial import guardar_carrera, obtener_historial_dia
+from src.infrastructure.logger import configurar_logger
 
 def cargar_tarifa():
     tarifa = Tarifa(0.02, 0.05)
@@ -17,6 +18,8 @@ def mostrar_menu():
 def main():
     tarifa = cargar_tarifa()
     carrera_activa = None
+    logger = configurar_logger()
+    logger.info("Aplicación iniciada por el usuario.")
 
     while True:
         mostrar_menu()
@@ -25,25 +28,32 @@ def main():
         if opcion == "1":
             if carrera_activa is not None:
                 print("Ya hay una carrera en curso.")
+                logger.warning("Ya hay una carrera en curso.")
             else:
                 carrera_activa = Carrera()
                 print("Carrera iniciada. Estado: parado")
+                logger.info("Carrera iniciada. Estado: parado")
 
         elif opcion == "2":
             if carrera_activa is None:
                 print("No hay ninguna carrera activa.")
+                logger.warning("No hay ninguna carrera activa.")
             else:
                 nuevo_estado = "movimiento" if carrera_activa.estado_actual == "parado" else "parado"
                 carrera_activa.cambiar_estado(nuevo_estado)
                 print(f"Nuevo estado: {nuevo_estado}")
+                logger.info(f"Cambio de estado a: {nuevo_estado}")
+
 
         elif opcion == "3":
             if carrera_activa is None:
                 print("No hay ninguna carrera activa.")
+                logger.warning("No hay ninguna carrera activa.")
             else:
                 carrera_activa.finalizar_carrera()
                 total = carrera_activa.calcular_total(tarifa)
                 print(f"Carrera finalizada. Total a cobrar: {total:.2f} €")
+                logger.info(f"Carrera finalizada. Total a cobrar: {total:.2f} €")
                 guardar_carrera(carrera_activa, tarifa)
                 carrera_activa = None
 
@@ -58,10 +68,12 @@ def main():
 
         elif opcion == "5":
             print("Cerrando aplicación. Hasta luego.")
+            logger.info("Aplicación cerrada por el usuario.")
             break
 
         else:
             print("Opción no válida, intenta de nuevo.")
+            logger.warning("Opción no válida seleccionada.")
 
 if __name__ == "__main__":
     main()
